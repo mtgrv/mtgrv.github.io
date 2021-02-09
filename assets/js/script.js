2a349778caec2649ed7df16a3bc366a31ccef972
matteogervasi <matteo.gervasi@me.com>
2021-02-09 17:21:12 +0100

    Add landing page

---
+++ assets/js/script.js
@@ -0,0 +1,26 @@
+/*-----------------------------------------------------------------------------------
+/*
+/* Init JS
+/*
+-----------------------------------------------------------------------------------*/
+
+setEmailHrefs()
+
+// --------------
+// EMAIL LINK
+// --------------
+function setEmailHrefs() {
+  var _rwObsfuscatedHref0 = "mai";
+  var _rwObsfuscatedHref1 = "lto";
+  var _rwObsfuscatedHref2 = ":ma";
+  var _rwObsfuscatedHref3 = "tt";
+  var _rwObsfuscatedHref4 = "eo@";
+  var _rwObsfuscatedHref5 = "d";
+  var _rwObsfuscatedHref6 = "sh";
+  var _rwObsfuscatedHref7 = "a";
+  var _rwObsfuscatedHref8 = "r";
+  var _rwObsfuscatedHref9 = "p.i";
+  var _rwObsfuscatedHref10 = "t";
+  var _rwObsfuscatedHref = _rwObsfuscatedHref0 + _rwObsfuscatedHref1 + _rwObsfuscatedHref2 + _rwObsfuscatedHref3 + _rwObsfuscatedHref4 + _rwObsfuscatedHref5 + _rwObsfuscatedHref6 + _rwObsfuscatedHref7 + _rwObsfuscatedHref8 + _rwObsfuscatedHref9 + _rwObsfuscatedHref10;
+  document.getElementById("email_text").href = _rwObsfuscatedHref;
+}
